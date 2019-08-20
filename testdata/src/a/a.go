d8759790debea53a8c199b00fc26ac7a959121cd
TSUYUSATO Kitsune <make.just.on@gmail.com>
2019-08-20 18:22:26 +0900

    fix: test case

--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -1,13 +1,13 @@
 package a
 
 import (
-	"github.com/MakeNowJust/enumcase/testdata/src/a/b"
+	"a/b"
 )
 
 func main() {
 	b1 := b.B1
 
-	switch b1 {
+	switch b1 { // want "Missing value: B2"
 	case b.B1:
 	case b.B3:
 	}
